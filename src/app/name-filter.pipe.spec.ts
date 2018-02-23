bf3066a3f87fb60310338f67003f0ca0139811dc
Jason Mowat <jmowat@digitalpraxis.com>
2018-02-23 16:51:42 -0600

    test: add bdd tests for name filter

--- src/app/name-filter.pipe.spec.ts
+++ src/app/name-filter.pipe.spec.ts
@@ -5,4 +5,22 @@ describe('NameFilterPipe', () => {
     const pipe = new NameFilterPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should allow everything through if field is empty', () => {
+  	const pipe = new NameFilterPipe();
+  	let values = [{name:'a'},{name:'b'},{name:'c'}];
+  	expect(pipe.transform(values, 'name', '')).toEqual(values);
+  });
+
+  it('should allow items to be filtered by name', () => {
+  	const pipe = new NameFilterPipe();
+  	let values = [{name:'a'},{name:'b'},{name:'c'}];
+  	expect(pipe.transform(values, 'name', 'a')).toEqual([{name:'a'}]);
+  });
+
+  it('should return nothing if there are no matches', () => {
+  	const pipe = new NameFilterPipe();
+  	let values = [{name:'a'},{name:'b'},{name:'c'}];
+  	expect(pipe.transform(values, 'name', 'd')).toEqual([]);
+  });
 });
