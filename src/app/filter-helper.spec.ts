e28a9982b71e98e3adc0c9a4625414b5d1508b04
Jason Mowat <jmowat@digitalpraxis.com>
2018-02-23 18:24:05 -0600

    test: add empty test for FilterHelper and create a component test for top-nav brand image being shown or not

---
+++ src/app/filter-helper.spec.ts
@@ -0,0 +1,8 @@
+import { FilterHelper } from './filter-helper';
+
+describe('FilterHelper', () => {
+  it('verify static class exists', () => {
+    expect(FilterHelper).toBeTruthy();
+  });
+
+});
--- src/app/top-nav/top-nav.component.spec.ts
+++ src/app/top-nav/top-nav.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 
 import { TopNavComponent } from './top-nav.component';
 
 describe('TopNavComponent', () => {
   let component: TopNavComponent;
   let fixture: ComponentFixture<TopNavComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +20,25 @@ describe('TopNavComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TopNavComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('.navbar-brand'));
+    el = de.nativeElement;
     fixture.detectChanges();
+    //console.log("de",de,"el",el);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the brand logo when showBrand is true',() => {
+    component.showBrand = true;
+    fixture.detectChanges();
+    expect(el.childElementCount).toEqual(1);
+  });
+
+  it('should hide the brand logo when showBrand is false', () => {
+    component.showBrand = false;
+    fixture.detectChanges();
+    expect(el.childElementCount).toEqual(0);
   });
 });
