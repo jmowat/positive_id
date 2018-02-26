0443ce766b18c5433da01ee8bf8f306a9bd7acf9
Jason Mowat <jmowat@digitalpraxis.com>
2018-02-26 07:52:21 -0600

    test: create isolated spec test for select-box-factory

---
+++ src/app/select-box-factory.spec.ts
@@ -0,0 +1,9 @@
+import { SelectBoxFactory } from './select-box-factory';
+
+describe('SelectBoxFactory', () => {
+  it('create an instance', () => {
+    const factory = new SelectBoxFactory();
+    expect(factory).toBeTruthy();
+  });
+
+});
