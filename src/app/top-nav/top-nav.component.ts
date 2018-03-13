7762b3baaf1ea843298a4a17b004099d7ee4027d
Jason Mowat <jmowat@digitalpraxis.com>
2018-03-12 22:48:10 -0500

    develop: change top nav bar options to generate quiz and drill respectively

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HeaderBannerComponent } from '../header-banner/header-banner.component';
+import { QuizParms } from '../quiz-main/game/quiz-parms';
+import { GameParmsService } from '../quiz-main/game/game-parms.service';
+import { GameBuilderStateService } from '../quiz-main/wizard/state/game-builder-state.service';
+import { QuizBuilderContext } from '../quiz-main/wizard/state/quiz-builder-context';
+import { DrillBuilderContext } from '../quiz-main/wizard/state/drill-builder-context';
 
 @Component({
   selector: 'app-top-nav',
@@ -9,9 +15,17 @@ export class TopNavComponent implements OnInit {
   @Input()
   public showBrand = false;
 
-  constructor() { }
+  constructor(private gameBuilderStateService: GameBuilderStateService) { }
 
   ngOnInit() {
+  }
+
+  generateQuiz() {
+    this.gameBuilderStateService.setContext(new QuizBuilderContext());
+  }
+
+  generateDrill() {
+    this.gameBuilderStateService.setContext(new DrillBuilderContext());
   }
 
 }
