5dca01b690a0f3a4d2855ce738fc496c72c0e6bd
Jason Mowat <jmowat@digitalpraxis.com>
2018-03-19 18:23:56 -0500

    chore: update reCaptcha for heroku

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,6 +5,8 @@
 
 export const sendmailUrl = `http://localhost:4300/sendmail`;
 // www.digitalpraxis.com
-export const publicSiteKey = '6Lf0pzoUAAAAAGDqvSIkYwb31CaAmSxXHDE6R0-M';
+// export const publicSiteKey = '6Lf0pzoUAAAAAGDqvSIkYwb31CaAmSxXHDE6R0-M';
+// positive-id.herokuapp.com
+export const publicSiteKey = '6LcBpk0UAAAAAHJ_NC92JdaZYskccK_vTc5A5lwN';
 // 127.0.0.1
 export const localhostSiteKey = '6LcAL00UAAAAAIODAAHMYC7wvPOMkILX2eAahDvG';
