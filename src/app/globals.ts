d848a842e699fe2b0af8c07453ffe09a0335aaf2
Jason Mowat <jmowat@digitalpraxis.com>
2018-03-19 18:36:20 -0500

    chore: switch production sendmail to heroku URL

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,7 +3,7 @@
 //
 'use strict';
 
-export const sendmailUrl = `http://localhost:4300/sendmail`;
+export const sendmailUrl = `https://positive-id.herokuapp.com:4300/sendmail`;
 // www.digitalpraxis.com
 // export const publicSiteKey = '6Lf0pzoUAAAAAGDqvSIkYwb31CaAmSxXHDE6R0-M';
 // positive-id.herokuapp.com
