57092da2cbed5ca63f3f21836ae6b28fabb57aca
Jason Mowat <jmowat@digitalpraxis.com>
2018-03-21 08:59:33 -0500

    version bump to v1.0.9

--- version.cs
+++ version.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.8")] 
-[assembly: System.Reflection.AssemblyFileVersion("1.0.8")] 
+[assembly: System.Reflection.AssemblyVersion("1.0.9")] 
+[assembly: System.Reflection.AssemblyFileVersion("1.0.9")] 
